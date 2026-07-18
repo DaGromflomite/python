ae962589daa96ebd5a2113b1d90796b68b4a51e7
DaGromflomite <timothyhrandall@proton.me>
2026-07-18 19:57:40 -0400

    Add files via upload

---
+++ band name gen/README.txt
@@ -0,0 +1,2 @@
+This is a band name generator done in python, its very simple, and the day 1 project in udemy's 100 days of code
+the program takes your city, and the name of your pet to generate a band name. 
---
+++ band name gen/bandname.py
@@ -0,0 +1,6 @@
+#band name generator, 100 days of code day 1 project
+print("welcome to the band name generator")
+city=input("what city did you grow up in?\n")
+pet=input("what is the name of your pet?\n")
+print("your band name is" + city + " " + pet)
+
