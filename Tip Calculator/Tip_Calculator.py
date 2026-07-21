155e7e9ce5b74d7a4acb94e7df37f9be34b3bccd
DaGromflomite <timothyhrandall@proton.me>
2026-07-20 22:43:04 -0400

    Add files via upload
    This is Udemys day two project from the 100 days of code.

---
+++ Tip Calculator/Tip_Calculator.py
@@ -0,0 +1,8 @@
+# This is the second project from 100 Days of Code. It Calculates the Tip For Dinning Out
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+amount = round((bill / people) * (1+(tip/100)),2)
+print(f"Each person should pay ${amount}")
